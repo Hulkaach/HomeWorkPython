4b0a80a883e6c0ad51ac28ac5c7184220e50b74d
unknown <hulkaach@mail.ru>
2023-03-10 18:50:21 +0300

    First work done

--- Ex_2.py
+++ Ex_2.py
@@ -2,3 +2,10 @@
 # *Пример:*
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+
+number = int(input('Введите число: '))
+sum = 0
+while number > 0:
+    sum = sum + number % 10
+    number = number // 10
+print(f"Сумма цифр введенного числа равна {sum}")
