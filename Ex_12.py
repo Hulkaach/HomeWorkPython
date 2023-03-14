f341712422c79346719b6f87e51d66b8dcc50969
unknown <hulkaach@mail.ru>
2023-03-14 23:21:52 +0300

    Second work done

--- Ex_12.py
+++ Ex_12.py
@@ -6,3 +6,25 @@
 # Пример
 # 4 4 -> 2 2
 # 5 6 -> 2 3
+
+import random
+import math
+num_X = random.randint(1, 1000)
+num_Y = random.randint(1, 1000)
+
+print(num_X,num_Y)
+sum_num = num_X + num_Y
+multiplication_num = num_X * num_Y
+
+half_sum = sum_num/2
+temp_x = multiplication_num-(half_sum*half_sum)
+if temp_x < 0:
+    temp_x *=-1
+
+x = half_sum-math.sqrt(temp_x)
+y = half_sum+math.sqrt(temp_x)
+
+print(f"Сумма цифр, загаданых Петей составляет - {sum_num}")
+print(f"Произведение цифр, загаданых Петей составляет - {multiplication_num}\n")
+print(f"Числа загаданные Петей это {int(x)} и {int(y)}\n") 
+print(f"Проверка {num_X} и {num_Y}") 
